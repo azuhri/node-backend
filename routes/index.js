907ae192fee55e36123e61698509a62a1b8de239
Azis Zuhri Pratomo <aziszuhrip354@gmail.com>
2023-08-17 16:21:46 +0700

    refactoring

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const app = require("express");
 const { testingFunction, createUser } = require("../handlers/app");
 
 const router = app.Router();
-const r = '/api/v1';
 
 router.get(`/testing`, testingFunction);
 router.post('/register', createUser);
